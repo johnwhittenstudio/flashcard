0479ee0d3729aea06141b8936bc2ceeb5dc757a5
John Whitten <johnwhitten.studio@gmail.com>
2021-10-30 18:58:47 -0700

    add new page to troubleshoot pop-up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#javascript-term").hide();
+    $("#javascript-definition").show();
+  });
+});
+
+$(document).ready(function() {
+  $("h5").click(function() {
+    alert("This is a heading.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
+
 function myFunction() {
   const element = document.body;
   element.classList.toggle("dark-mode");
 }
-
-jQuery("h1").click(function() {
-  alert("This is a heading.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
-});
